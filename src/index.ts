2457b9cfdb1c433c4a296c56d5ab5337ad586076
Leon Adler <git@leon-adler.com>
2019-05-25 16:00:11 +0200

    Add index file to use as npm package

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { Inject } from "./inject";
+export { Injectable } from "./injectable";
+export { InjectionError } from "./injection-error";
+export { InjectionToken } from "./injection-token";
+export { Injector } from "./injector";
+export { Provider } from "./provider";
